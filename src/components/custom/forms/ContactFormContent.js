f104dba9054144ed468c954e9736e9feb827e208
robMolloy <robmolloy@hotmail.co.uk>
2021-01-13 16:04:32 +0000

    validation added

--- src/components/custom/forms/ContactFormContent.js
+++ src/components/custom/forms/ContactFormContent.js
@@ -3,20 +3,94 @@ import GridContainer from "../../generic/grids/GridContainer";
 import GridItem from "../../generic/grids/GridItem";
 import Input from "../../generic/fields/Input";
 
+import { yupResolver } from "@hookform/resolvers/yup";
+import { useForm } from "react-hook-form";
+import * as yup from "yup";
+import "yup-phone";
+
+const Schema = yup.object().shape({
+  type: yup
+    .string()
+    .required("Shtick it in my box")
+    .oneOf(["landline", "mobile"]),
+  phone_number: yup
+    .string()
+    .required("Phone number is a required field")
+    .phone("", false, "Must be a valid phone number"),
+});
+
+const getDefaultFieldProps = ({ form, name }) => ({
+  name,
+  ref: form.register,
+  helperText: form.errors?.[name]?.message,
+  error: !!form.errors[name],
+});
+
 const ContactFormContent = (props = {}) => {
   // let id, values, formControls, setFormControls;
   // ({ id, values, formControls, setFormControls } = props);
 
+  const form = useForm({ resolver: yupResolver(Schema), mode: "onChange" });
   return (
     <GridContainer>
       <GridItem xs={12} sm={6}>
-        <Input label="Type" />
+        <Input label="Type" {...getDefaultFieldProps({ form, name: "type" })} />
       </GridItem>
       <GridItem xs={12} sm={6}>
-        <Input label="Phone Number" />
+        <Input
+          label="Phone Number"
+          {...getDefaultFieldProps({ form, name: "phone_number" })}
+        />
       </GridItem>
     </GridContainer>
   );
 };
 
 export default ContactFormContent;
+
+/*
+import React from "react";
+import GridContainer from "../../generic/grids/GridContainer";
+import GridItem from "../../generic/grids/GridItem";
+import Input from "../../generic/fields/Input";
+
+const Schema = yup.object().shape({
+  type: yup
+    .string()
+    .required("Shtick it in my box")
+    .min(3, "needs to be longer, Silad! :(")
+    .max(10, "I can't handle something that big, Silad!"),
+  phone_number: yup
+    .string()
+    .required("Phone number is a required field")
+    .phone("", false, "Must be a valid phone number"),
+});
+
+const getDefaultFieldProps = ({ form, name }) => ({
+  name,
+  ref: form.register,
+  helperText: form.errors?.[name]?.message,
+  error: !!form.errors[name],
+});
+
+const ContactFormContent = (props = {}) => {
+  const form = useForm({ resolver: yupResolver(Schema), mode: "onChange" });
+
+  return (
+    <GridContainer>
+      <GridItem xs={12} sm={6}>
+        <Input label="Type" {...getDefaultFieldProps({ form, name: "type" })} />
+      </GridItem>
+      <GridItem xs={12} sm={6}>
+        <Input
+          label="Phone Number"
+          {...getDefaultFieldProps({ form, name: "phone_number" })}
+        />
+      </GridItem>
+    </GridContainer>
+  );
+};
+
+export default ContactFormContent;
+
+*/
--- src/components/custom/forms/CustomerFormContent.js
+++ src/components/custom/forms/CustomerFormContent.js
@@ -3,17 +3,50 @@ import GridContainer from "../../generic/grids/GridContainer";
 import GridItem from "../../generic/grids/GridItem";
 import Input from "../../generic/fields/Input";
 
+import { yupResolver } from "@hookform/resolvers/yup";
+import { useForm } from "react-hook-form";
+import * as yup from "yup";
+
+const Schema = yup.object().shape({
+  first_name: yup
+    .string()
+    .required("Shtick it in my box")
+    .min(3, "needs to be longer, Silad! :(")
+    .max(10, "I can't handle something that big, Silad!"),
+  last_name: yup
+    .string()
+    .required("Shtick it in my box")
+    .min(3, "needs to be longer, Silad! :(")
+    .max(10, "I can't handle something that big, Silad!"),
+});
+
+const getDefaultFieldProps = ({ form, values = {}, name }) => ({
+  name,
+  ref: form.register,
+  helperText: form.errors?.[name]?.message,
+  error: !!form.errors[name],
+  defaultValue: values[name] ?? "",
+});
+
 const CustomerFormContent = (props = {}) => {
   // let id, values, formControls, setFormControls;
   // ({ id, values, formControls, setFormControls } = props);
 
+  const form = useForm({ resolver: yupResolver(Schema), mode: "onChange" });
+
   return (
     <GridContainer>
       <GridItem xs={12} sm={6}>
-        <Input label="First Name" />
+        <Input
+          label="First Name"
+          {...getDefaultFieldProps({ form, name: "first_name" })}
+        />
       </GridItem>
       <GridItem xs={12} sm={6}>
-        <Input label="Last Name" />
+        <Input
+          label="Last Name"
+          {...getDefaultFieldProps({ form, name: "last_name" })}
+        />
       </GridItem>
     </GridContainer>
   );
